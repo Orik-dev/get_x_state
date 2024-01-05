cf06d933689e530de46f468a9b0f14169eefaa4f
Orkhan Guseinov <orxan4ik7777@gmail.com>
2024-01-05 17:21:37 +0400

    floactionbtn controller

--- lib/controllers/cart_controller.dart
+++ lib/controllers/cart_controller.dart
@@ -4,6 +4,8 @@ import 'package:get_x_state/models/product.dart';
 class CartController extends GetxController {
   var cartItems = <Product>[].obs; // List of Cart items
 
+  int get count => cartItems.length;
+
   double get totalPrice => cartItems.fold(0, (sum, item) => sum + item.price);
 
   void addToCart(Product product) {
--- lib/views/shopping_page.dart
+++ lib/views/shopping_page.dart
@@ -12,62 +12,79 @@ class ShoppingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
-      body: Column(
-        children: [
-          Expanded(child: GetX<ShoppingController>(builder: (controller) {
-            return ListView.builder(
-              itemCount: controller.products.length,
-              itemBuilder: (context, index) => Card(
-                margin: const EdgeInsets.all(12),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                controller.products[index].productName,
-                                style: const TextStyle(fontSize: 24),
-                              ),
-                              Text(controller
-                                  .products[index].productDescription),
-                            ],
-                          ),
-                          Text('\$${controller.products[index].price}',
-                              style: const TextStyle(fontSize: 24)),
-                        ],
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          cartController.addToCart(controller.products[index]);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue, // Цвет фона кнопки
-                          foregroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(
+          Icons.add_shopping_cart,
+          size: 24,
+        ),
+        onPressed: () {},
+        backgroundColor: Colors.amber,
+        label: GetX<CartController>(builder: (controller) {
+          return Text(
+            controller.count.toString(),
+            style: const TextStyle(color: Colors.black, fontSize: 24),
+          );
+        }),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(child: GetX<ShoppingController>(builder: (controller) {
+              return ListView.builder(
+                itemCount: controller.products.length,
+                itemBuilder: (context, index) => Card(
+                  margin: const EdgeInsets.all(12),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  controller.products[index].productName,
+                                  style: const TextStyle(fontSize: 24),
+                                ),
+                                Text(controller
+                                    .products[index].productDescription),
+                              ],
+                            ),
+                            Text('\$${controller.products[index].price}',
+                                style: const TextStyle(fontSize: 24)),
+                          ],
                         ),
-                        child: const Text("Add to cart"),
-                      ),
-                    ],
+                        ElevatedButton(
+                          onPressed: () {
+                            cartController
+                                .addToCart(controller.products[index]);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue, // Цвет фона кнопки
+                            foregroundColor: Colors.white,
+                          ),
+                          child: const Text("Add to cart"),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            );
-          })),
-          GetX<CartController>(builder: (controller) {
-            return Text(
-              'Total amount: \$ ${controller.totalPrice}',
-              style: const TextStyle(color: Colors.white, fontSize: 25),
-            );
-          }),
-          const SizedBox(
-            height: 100,
-          )
-        ],
+              );
+            })),
+            GetX<CartController>(builder: (controller) {
+              return Text(
+                'Total amount: \$ ${controller.totalPrice}',
+                style: const TextStyle(color: Colors.white, fontSize: 25),
+              );
+            }),
+            const SizedBox(
+              height: 100,
+            )
+          ],
+        ),
       ),
     );
   }
